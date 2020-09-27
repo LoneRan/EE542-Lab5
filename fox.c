dc7c7a7d265d164a378ffecd9d426658d020e282
LoneRan <48748710+LoneRan@users.noreply.github.com>
2020-09-26 19:09:55 -0700

    Update fox.c

--- fox.c
+++ fox.c
@@ -29,6 +29,61 @@ typedef struct {
 	int my_rank; /* rank within the grid */
 }GridInfo;
 
+int check(double matrixA[N][N], double matrixB[N][N], double matrixC[N][N]){
+	double r[N][1];
+	int i, j, k;
+	for (i = 0; i < N; i++)
+    {
+        r[i][0] = rand() % 2;
+    }
+
+    double br[N][1];
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < 1; j++)
+            {
+                for (k = 0; k < N; k++)
+                    {
+                        br[i][j] = br[i][j] + matrixB[i][k] * r[k][j];
+                    }
+            }
+    }
+    double cr[N][1];
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < 1; j++)
+            {
+                for (k = 0; k < N; k++)
+                    {
+                        cr[i][j] = cr[i][j] + matrixC[i][k] * r[k][j];
+                    }
+            }
+    }
+    double abr[N][1];
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < 1; j++)
+            {
+                for (k = 0; k < N; k++)
+                    {
+                        abr[i][j] = abr[i][j] + matrixA[i][k] * br[k][j];
+                    }
+            }
+    }
+    for (i = 0; i < N; i++)
+    {
+        abr[i][0] -= cr[i][0];
+    }
+    int flag = 1;
+    for (i = 0; i < N; i++)
+    {
+        if (abr[i][0] == 0)
+            continue;
+        else
+            flag = 0;
+    }
+    return flag;
+}
 
 void SetupGrid(GridInfo *grid)
 {
