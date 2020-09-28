263ea99fa38839d704da9f7b0bf425679bb9ff4a
LoneRan <48748710+LoneRan@users.noreply.github.com>
2020-09-28 13:04:18 -0700

    Update checkMatrix.h

--- checkMatrix.h
+++ checkMatrix.h
@@ -3,18 +3,28 @@
 #include <stdlib.h>
 
 
-#define N 3
+#define N 2
 
 // using Freivald’s Algorithm to check if matrix is correct, "1" means correct, "0" means wrong
 int check(double a[N][N], double b[N][N], double c[N][N]){
-	double r[N][1];
+	//double r[N][1];
+    double **r;
+    r = (double **)malloc(N*sizeof(double*));
+    for(int i = 0; i < N; i++){
+        r[i] = (double*)malloc(sizeof(double));
+    }
     int i,j,k;
 	for (i = 0; i < N; i++)
     {
         r[i][0] = rand() % 2;
     }
 
-    double br[N][1];
+    //ouble br[N][1];
+    double **br;
+    br = (double **)malloc(N*sizeof(double*));
+    for(int i = 0; i < N; i++){
+        br[i] = (double*)malloc(sizeof(double));
+    }
     for (i = 0; i < N; i++)
     {
         for (j = 0; j < 1; j++)
@@ -25,7 +35,12 @@ int check(double a[N][N], double b[N][N], double c[N][N]){
                     }
             }
     }
-    double cr[N][1];
+    //double cr[N][1];
+    double **cr;
+    cr = (double **)malloc(N*sizeof(double*));
+    for(int i = 0; i < N; i++){
+        cr[i] = (double*)malloc(sizeof(double));
+    }
     for (i = 0; i < N; i++)
     {
         for (j = 0; j < 1; j++)
@@ -36,7 +51,12 @@ int check(double a[N][N], double b[N][N], double c[N][N]){
                     }
             }
     }
-    double abr[N][1];
+    //double abr[N][1];
+    double **abr;
+    abr = (double **)malloc(N*sizeof(double*));
+    for(int i = 0; i < N; i++){
+        abr[i] = (double*)malloc(sizeof(double));
+    }
     for (i = 0; i < N; i++)
     {
         for (j = 0; j < 1; j++)
@@ -54,7 +74,7 @@ int check(double a[N][N], double b[N][N], double c[N][N]){
     int flag = 1;
     for (i = 0; i < N; i++)
     {
-        if (abr[i][0] == 0)
+        if (abr[i][0] <= 0.000005)
             continue;
         else
             flag = 0;
@@ -63,9 +83,9 @@ int check(double a[N][N], double b[N][N], double c[N][N]){
 }
 
 int main(){
-    double a[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
-    double b[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
-    double c[3][3] = {{30,36,42},{66,81,96},{102,126,150}};
+    double a[2][2] = {{1.234567,2.234567},{3.234567,4.234567}};
+    double b[2][2] = {{1.234567,2.234567},{3.234567,4.234567}};
+    double c[2][2] = {{8.75201,12.221146},{17.690280,25.159414}};
     int res = check(a,b,c);
     printf("%d\n",res);
 }
