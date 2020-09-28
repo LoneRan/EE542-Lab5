69a05b055302fa6ea207e933dfc6351b49a673dd
LoneRan <48748710+LoneRan@users.noreply.github.com>
2020-09-28 00:53:57 -0700

    Update checkMatrix.h

--- checkMatrix.h
+++ checkMatrix.h
@@ -3,11 +3,12 @@
 #include <stdlib.h>
 
 
-#define N 14000
+#define N 3
 
 // using Freivald’s Algorithm to check if matrix is correct, "1" means correct, "0" means wrong
-int check(double matrixA[N][N], double matrixB[N][N], double matrixC[N][N]){
+int check(double a[N][N], double b[N][N], double c[N][N]){
 	double r[N][1];
+    int i,j,k;
 	for (i = 0; i < N; i++)
     {
         r[i][0] = rand() % 2;
@@ -59,4 +60,12 @@ int check(double matrixA[N][N], double matrixB[N][N], double matrixC[N][N]){
             flag = 0;
     }
     return flag;
+}
+
+int main(){
+    double a[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    double b[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    double c[3][3] = {{30,36,42},{66,81,96},{102,126,150}};
+    int res = check(a,b,c);
+    printf("%d\n",res);
 }
